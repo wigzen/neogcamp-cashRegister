1b2ab11bd816bda6daaba10dced881015525fdd1
wigzen <61306958+wigzen@users.noreply.github.com>
2022-10-14 17:40:17 +0530

    Improvement :Code revamped

--- index.js
+++ index.js
@@ -1,38 +1,63 @@
-const billAmount = document.querySelector("#bill-amount");
-const cashGiven = document.querySelector("#cash-given");
-const checkButton = document.querySelector("#check-button");
-const message = document.querySelector("#error-message");
-const noOfNotes = document.querySelectorAll(".no-of-notes");
+let btnPrimary = document.getElementById("check-button");
+let billAmount = document.getElementById("bill-amount");
+let cashGiven = document.getElementById("cash-given");
+let message = document.getElementById("error-message");
+let noOfNotes = document.querySelectorAll(".no-of-notes");
+let table = document.getElementById("table");
+let availableNotes = ["2000", "500", "200", "100", "50", "20", "10", "1"];
 
-const Notes = [2000, 500, 100, 20, 10, 5, 1];
-let arr= new Map()
-checkButton.addEventListener("click", function validateBillAndCashAmount() {
+btnPrimary.addEventListener("click", () => {
+  let newBillAmount = Number(billAmount.value);
+  let newCashGiven = Number(cashGiven.value);
   hideMessage();
-  if (billAmount.value > 0 && cashGiven.value > 0  ) {
-    if (Number(cashGiven.value) > Number(billAmount.value)) {
-      const amountToBeReturned = cashGiven.value - billAmount.value; // 2022 - 12 = 2010
+  if (newBillAmount > 0) {
+    if (newCashGiven > newBillAmount) {
+      showTable(table);
+      let amountToBeReturned = newCashGiven - newBillAmount;
       calculateChange(amountToBeReturned);
+      showMessage("Thanks! Come back again😊");
+    } else if (
+      newBillAmount > newCashGiven &&
+      newCashGiven != "" &&
+      newCashGiven > 0
+    ) {
+      showMessage("Do you want to wash plates ?🧼🍽️");
+      hideTable(table);
+    } else if (newCashGiven < 0) {
+      hideMessage();
+      hideTable(table);
+      showMessage(" I need Actual Cash Man😒");
+    } else if (newCashGiven === newBillAmount) {
+      hideTable(table);
+      showMessage("Thanks! Come back again😊");
     } else {
-      showMessage("Please check your wallet");
+      hideMessage();
+      hideTable(table);
+      showMessage("Gimme Cash 🖐️💸");
     }
   } else {
-    showMessage("Invalid Bill Amount");
+    showMessage("Invalid Bill Amount❌");
+    hideTable(table);
   }
 });
-function calculateChange(amountToBeReturned) {
-  for (let i = 0; i < Notes.length; i++) {
-    const numberOfNotes = Math.trunc(amountToBeReturned / Notes[i]);
-    amountToBeReturned = amountToBeReturned % Notes[i];
-    noOfNotes[i].innerText = numberOfNotes;
-    arr.set(Notes[i],numberOfNotes)
-  }
-}
-console.log(arr)
+
 function hideMessage() {
   message.style.display = "none";
 }
-
+function hideTable(table) {
+  table.style.display = "none";
+}
+function showTable(table) {
+  table.style.display = "flex";
+}
 function showMessage(msg) {
   message.style.display = "block";
   message.innerText = msg;
+}
+function calculateChange(amountToBeReturned) {
+  for (let i = 0; i < availableNotes.length; i++) {
+    let numberOfNotes = Math.trunc(amountToBeReturned / availableNotes[i]);
+    amountToBeReturned %= availableNotes[i];
+    noOfNotes[i].innerText = numberOfNotes;
+  }
 }
